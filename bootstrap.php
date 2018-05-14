c551da203b27541f92abfc3375cf7167547feca6
Pascal Nitsche <saikootau@gmail.com>
2018-05-14 19:47:24 +0200

    [chore] Remove deprecated AnnotationRegistry call from test bootstrapping

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,4 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-AnnotationRegistry::registerAutoloadNamespace(
-    'JMS\Serializer\Annotation',
-    [ __DIR__.'/vendor/jms/serializer/src' ]
-);
+AnnotationRegistry::registerLoader('class_exists');
